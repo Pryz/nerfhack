ec311f3d665017ae6333bc8f600bdd2a7011ae63
Julien Fabre <julien.fabre@tubemogul.com>
2014-08-14 23:28:58 -0700

    Fix create table

--- rest-api/setup.py
+++ rest-api/setup.py
@@ -8,9 +8,9 @@ con = lite.connect('nerf.db')
 
 with con:
     cur = con.cursor()    
-    cur.execute("CREATE TABLE IF NOT EXISTS Games(
+    cur.execute("""CREATE TABLE IF NOT EXISTS Games(
         Id INTEGER PRIMARY KEY, 
         Player1 TEXT,
         Player2 TEXT,
         Score1 INT,
-        Score2 INT)")
+        Score2 INT)""")
