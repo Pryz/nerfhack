cd7dd90d92c849536dd10e5ccaf5c3064f5b5c35
Chintan Jain <chintan.jain@tubemogul.com>
2014-08-15 08:44:35 -0700

    Discarding undefined variables

--- ui/bootstrap/js/main.js
+++ ui/bootstrap/js/main.js
@@ -80,24 +80,29 @@ $(document).ready(function() {
                  "Score2": 0
                  */
                 console.log('Player 1 Score : ' + response.Score1);
+                var player1Score = (typeof response.Score1 === 'undefined') ? 0 : response.Score1;
+                console.log('updated Player 1 Score : ' + player1Score);
+                
+                if (player1Score == 16) {
+                    winningMessage(1);
+                    clearTimeout(myTimeout);
+                    return;
+                } 
+                    
                 if (mode === 'double') {
                     console.log('Player 2 Score : ' + response.Score2);
-                    if (response.Score1 == 16) {
-                        winningMessage(1);
-                        clearTimeout(myTimeout);
-                        return;
-                    } 
-                    
-                    if (response.Score2 == 16) {
+                    var player2Score = (typeof response.Score2 === 'undefined') ? 0 : response.Score2;
+                    console.log('Updated Player 2 Score : ' + player2Score);
+                    if (player2Score == 16) {
                         winningMessage(2);
                         clearTimeout(myTimeout);
                         return;
                     }
                 }
 
-                $('#player1-score').html(("0" + response.Score1).slice(-2));
+                $('#player1-score').html(("0" + player1Score).slice(-2));
                 if (mode === 'double') {
-                    $('#player2-score').html(("0" + response.Score2).slice(-2));
+                    $('#player2-score').html(("0" + player2Score).slice(-2));
                 }
             }
         });
